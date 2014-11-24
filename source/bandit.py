40371baeecb59b5368eb680aaaca839802673f3c
polysalama <zigaresnik@gmail.com>
2014-11-24 02:23:16 +0100

    bandit

---
+++ source/bandit.py
@@ -0,0 +1,34 @@
+import urllib.request
+from scipy.stats import beta
+import sys
+
+
+def bandit(url):
+    #stevec zmag
+    wins = 0
+    #stevilo avtomatov in potegov
+    num_of_machines = int(urllib.request.urlopen(url + '/machines').read())
+    num_of_pulls = int(urllib.request.urlopen(url + '/pulls').read())
+    #stevec zmag posameznega avtomata
+    machines = [0] * num_of_machines
+    for j in range(num_of_pulls):
+        samples = []
+        #vzorcimo vsak avtomat iz njegove apriorne beta distribucije
+        for k in range(num_of_machines):
+            prior = beta(machines[k] + 1, 1 + j - machines[k])
+            samples += [prior.rvs()]
+        #izberemo avtomat z najvecjo vrednosjo vzorca
+        selected = samples.index(max(samples))
+        #potegnemo izbran avtomat, poveca se stevec zmag avtomata in stevec skupnih uspesnih potegov
+        pull = int(urllib.request.urlopen(url + "/" + str(selected + 1) + "/" + str(j + 1)).read())
+        machines[selected] += pull
+        wins += pull
+    return wins
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(bandit(sys.argv[1]))
